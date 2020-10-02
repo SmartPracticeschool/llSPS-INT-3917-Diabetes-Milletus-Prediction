d68487bc7b1ecdf534a9717ade06e379edca89f1
nandiniballa <72250002+nandiniballa@users.noreply.github.com>
2020-10-02 23:07:14 +0530

    Add files via upload

---
+++ flask/app.py
@@ -0,0 +1,44 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from joblib import load
+
+app = Flask(__name__)
+model = pickle.load(open('strength.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = [[float(x) for x in request.form.values()]]
+    #print(x_test)
+    #sc = load('scaler.save')
+    
+    
+    prediction = model.predict(x_test)
+    print(prediction)
+    output=prediction[0]
+    if(output==1):
+        pred="will not have diabetes disease"
+    else:
+        pred="will have diabetes disease"
+    
+    return render_template('index.html', prediction_text='User {}'.format(pred))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.y_predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+if __name__ == "__main__":
+    app.run(debug=True)
